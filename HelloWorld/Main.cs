ff3b5182f8cbc97b747748ac17c3174771422bc7
hanabi1224 <harlowmoo@gmail.com>
2020-11-21 16:41:05 +0800

    HelloWorld mod

---
+++ HelloWorld/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+using Harmony12;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace HelloWorld
+{
+    public static class Main
+    {
+        private static Settings _settings;
+        private static UnityModManager.ModEntry.ModLogger _logger;
+
+        private static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            HarmonyInstance.Create(modEntry.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+            _logger = modEntry.Logger;
+            _settings = UnityModManager.ModSettings.Load<Settings>(modEntry);
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            return true;
+        }
+
+        private static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginVertical("Box", (GUILayoutOption[])(object)new GUILayoutOption[0]);
+            GUILayout.Label($"测试MOD, 读取当前游戏版本:{MainMenu.instance.gameVersionText.text}");
+            GUILayout.EndVertical();
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            if (!value)
+            {
+                return false;
+            }
+            _settings.Enabled = value;
+            return true;
+        }
+
+        private static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            _settings.Save(modEntry);
+        }
+    }
+}
---
+++ HelloWorld/Settings.cs
@@ -0,0 +1,14 @@
+﻿using UnityModManagerNet;
+
+namespace HelloWorld
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public bool Enabled { get; set; }
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
